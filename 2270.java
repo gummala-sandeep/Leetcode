b407bef2b81af2105d41dc96199c5b8310aae630
gummala-sandeep <gummalasandy191212@gmail.com>
2025-01-03 17:49:23 +0530

    Create 2270.java

---
+++ 2270.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+    long sum=0,count=0,res=0,numsSize=nums.length;
+    for(int i=0;i<numsSize;i++){
+        sum+=nums[i];
+    }
+    for(int j=0;j<numsSize-1;j++){
+        res+=nums[j];
+        if(res>=sum-res){
+            count++;
+        }       
+    }
+    return (int)count;
+    }
+}
