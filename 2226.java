7f0394baf6900ec4e57a1abce2c95030f62985af
Gummala Sagar Sandeep Kumar <gummalasandy191212@gmail.com>
2025-03-14 14:38:56 +0530

    Create 2226.java

---
+++ 2226.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maximumCandies(int[] candies, long k) {
+        int avg=0,n=candies.length,low=1,high=0,ans=0;
+        long sum=0;
+        for(int i:candies){
+            sum+=i;
+            high=Math.max(high,i);
+         }
+        if(sum<k){ return 0;}
+        low=1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            long count=0;
+            for(int i:candies)
+                count+=i/mid;
+            if(count>=k){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1; 
+            }
+        }
+        return ans;
+    }
+}
